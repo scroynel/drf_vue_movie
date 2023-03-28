c32978dd96fad997c58390747e26169b1d489513
Vladislav <scroynel@gmail.com>
2023-03-28 21:09:18 +0200

    drf with vue.js movie

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <Nav />
     <router-view/>
+    <Footer />
   </div>
 </template>
 
+
+<script>
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+export default {
+  components: {Nav, Footer}
+}
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  @import "assets/css/bootstrap.css";
+  @import "assets/css/font-awesome.css";
+  @import "assets/css/style.css";
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Single from '../views/Single.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/:id',
+    name: 'Single',
+    component: Single
   },
   {
     path: '/about',
